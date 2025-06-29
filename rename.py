571ea77e6e98b30b8b2511570ec80d6df451b0d1
krantawan <krantawan@outlook.com>
2025-06-29 20:27:15 +0700

    feat(operator): rename model skin operator to lowercase

--- rename.py
+++ rename.py
@@ -4,18 +4,18 @@ def rename_in_directory(base_dir):
     for root, dirs, files in os.walk(base_dir, topdown=False):
         # Rename files first
         for name in files:
-            if "#" in name:
+            if "#" in name or any(c.isupper() for c in name):
                 old_path = os.path.join(root, name)
-                new_name = name.replace("#", "_")
+                new_name = name.replace("#", "_").lower()
                 new_path = os.path.join(root, new_name)
                 print(f"Renaming file: {old_path} -> {new_path}")
                 os.rename(old_path, new_path)
 
         # Rename directories
         for name in dirs:
-            if "#" in name:
+            if "#" in name or any(c.isupper() for c in name):
                 old_path = os.path.join(root, name)
-                new_name = name.replace("#", "_")
+                new_name = name.replace("#", "_").lower()
                 new_path = os.path.join(root, new_name)
                 print(f"Renaming folder: {old_path} -> {new_path}")
                 os.rename(old_path, new_path)
